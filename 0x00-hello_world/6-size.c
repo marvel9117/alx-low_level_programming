f4e38894f9c940eeee70f60475e589044c1c84d6
marvel <djmarvel@yahho.com>
2023-08-10 07:50:33 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 /**
-*main - main function
-*
-*
-*Return: return 0
-*/
-int main(void);
+ *main -is the entry point for the program.
+ *
+ *Return:zero if no error, Non-zero value  if errors.
+ */
+int main(void)
 {
-	char a;
-	int b;
-	long int c;
-	long long int d;
-	float f;
-
-	printf("Size of char: %ld byte(s)\n", sizeoff(a));
-	printf("Size of int: %ld byte(s)\n", sizeoff(b));
-	printf("Size of long int: %ld byte(s)\n", sizeoff(c));
-	printf("Size of long long int: %ld byte(s)\n", sizeoff(d));
-	printf("Size of float: %ld bytes(s)\n", sizeoff(f));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
