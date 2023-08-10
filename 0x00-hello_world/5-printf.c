20882844d83f3002f04fa2303873f0c18d0cd45c
marvel <djmarvel@yahho.com>
2023-08-10 06:50:58 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A program that prints out using printf
+ *
+ * Return - when return is equal to zero it means code is successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
