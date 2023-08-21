afcbeb464e85da29622a7b25ca4aa0cbcbde1b4e
marvel <djmarvel@yahho.com>
2023-08-21 04:09:13 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.
+ * @n: is the parameter
+ * Return: 0 (success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Readme forpointers array string
