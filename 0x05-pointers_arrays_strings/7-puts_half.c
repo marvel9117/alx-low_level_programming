db063e219f8daba2a70f501c330782b8a70e45cd
marvel <djmarvel@yahho.com>
2023-08-21 05:34:01 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half -   prints half of a string, followed by a new line.
+ * @str: char pointer
+ * Return: the length *s
+ */
+void puts_half(char *str)
+{
+	int len, i;
+
+	len = strlen(str);
+	if (len % 2 != 0)
+		i = (len / 2) + 1;
+	else
+		i = len / 2;
+	for (; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
