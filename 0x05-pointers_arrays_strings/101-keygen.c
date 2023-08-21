7a2107449ea11d9d6087a4fbc42a6b9e00349a07
marvel <djmarvel@yahho.com>
2023-08-21 05:50:09 -0700

    final

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,114 +2,41 @@
 #include <stdlib.h>
 #include <time.h>
 
-/*Function to shuffle an array*/
-void shuffleArray(char *array[], int size)
+/**
+ * main - random password generator for 101-crackme
+ *
+ * Return: 0.
+ */
+
+int main(void)
 {
-	int i;
+	int a, j, b, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
+
 	srand(time(NULL));
-	if (size > 1)
+	while (s != 2772)
 	{
-		for (i = size - 1; i > 0; --i)
+		a = b = s = 0;
+		while ((2772 - 122) > s)
+		{	j = rand() % 62;
+			p[a] = c[j];
+			s += c[j];
+			a++;
+		}
+		while (c[b])
 		{
-			int j = rand() % (i + 1);
-			char *temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
+			if (c[b] == (2772 - s))
+			{
+				p[a] = c[b];
+				s += c[b];
+				a++;
+				break;
+			}
+			b++;
 		}
 	}
-}
-
-int checksum(const char *data) {
-    int sum = 0;
-    int i = 0;
-
-    while (data[i] != '\0') {
-        sum += (int)data[i];
-        i++;
-    }
-
-    return sum;
-}
-
-int main()
-{
-	int numStrings;
-	/*Array of strings*/
-	const char *strings[] = {
-		"zzzzzzzzzzzzzzzzzzzzzzX",
-		"zzzzzzzzzzzzzzzzzzzzzXz",
-		"zzzzzzzzzzzzzzzzzzzzXzz",
-		"zzzzzzzzzzzzzzzzzzzXzzz",
-		"zzzzzzzzzzzzzzzzzzXzzzz",
-		"zzzzzzzzzzzzzzzzzXzzzzz",
-		"zzzzzzzzzzzzzzzzXzzzzzz",
-		"zzzzzzzzzzzzzzzXzzzzzzz",
-		"zzzzzzzzzzzzzzXzzzzzzzz",
-		"zzzzzzzzzzzzzXzzzzzzzzz",
-		"zzzzzzzzzzzzXzzzzzzzzzz",
-		"zzzzzzzzzzzXzzzzzzzzzzz",
-		"zzzzzzzzzzXzzzzzzzzzzzz",
-		"zzzzzzzzzXzzzzzzzzzzzzz",
-		"zzzzzzzzXzzzzzzzzzzzzzz",
-		"zzzzzzzXzzzzzzzzzzzzzzz",
-		"zzzzzzXzzzzzzzzzzzzzzzz",
-		"zzzzzXzzzzzzzzzzzzzzzzz",
-		"zzzzXzzzzzzzzzzzzzzzzzz",
-		"zzzXzzzzzzzzzzzzzzzzzzz",
-		"zzXzzzzzzzzzzzzzzzzzzzz",
-		"zXzzzzzzzzzzzzzzzzzzzzz",
-		"Xzzzzzzzzzzzzzzzzzzzzzz",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*A",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AAA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"AA*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"A*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-		"*AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"};
-	
-	/*Get the length of the array*/
-	numStrings = sizeof(strings) / sizeof(strings[0]);
-
-	/*Shuffle the array*/
-	shuffleArray((char **)strings, numStrings);
-
-	/*Print the randomly selected string*/
-	printf("%s", strings[0]);
-
-	return 0;
+	p[a] = '\0';
+	printf("%s\n", p);
+	return (0);
 }
