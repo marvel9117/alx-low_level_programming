d88128ae0d377f705d02fda8eb37ab7a98f4160d
marvel <djmarvel@yahho.com>
2023-09-01 16:39:26 -0700

    100 chmage

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,20 +1,16 @@
-#include <stdio.h>
-#include "main.h"
+#include<stdio.h>
 
 /**
- * main - prints all arguments it receives
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - function
  *
- * Return: Always 0 (Success)
+ * @argv: the chaine of chaine
+ * @argc: the int
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
-
-	return (0);
+int i;
+for (i = 0; i < argc; i++)
+printf("%s\n", argv[i]);
+return (0);
+}
